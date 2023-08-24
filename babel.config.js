9c2e9906816a20f103c90b9cfec6d23a703c7a37
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-08-24 21:18:49 +0200

    build: add path alias for src directory to avoid long relative paths

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,16 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["expo-router/babel"],
+    plugins: [
+      "expo-router/babel",
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@": "./src",
+          },
+        },
+      ],
+    ],
   };
 };
